0942781564ee9a8f809773feafdee7c2ceeb6fb0
SURI-1602151542\Administrator <myss3755@gmail.com>
2021-10-04 18:45:48 +0900

    리펙토링 vuex

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput><!--v-on:하위컴포넌트에서 발생시킨 이벤트 이름 ="현재 컴포넌트의 메서드명" -->
-    <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList><!-- v-bind:내려보낼 프롭스 속성 이름="현재 위치의 컴포넌트 데이터 속성" -->
-    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
+    <TodoInput></TodoInput><!--v-on:하위컴포넌트에서 발생시킨 이벤트 이름 ="현재 컴포넌트의 메서드명" -->
+    <TodoList></TodoList><!-- v-bind:내려보낼 프롭스 속성 이름="현재 위치의 컴포넌트 데이터 속성" -->
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
@@ -14,48 +14,7 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  data(){
-    return {
-      todoItems: []
-    }
-  },
-  methods: {
-    addOneItem(todoItem){
-            const obj = {complete: false, item: todoItem};
-            console.log(todoItem);
-            //저장하는 로직
-            //localStorage.setItem(this.newTodoItem, obj);//API 1가지 오브젝트로 떨어져서 어떤값인지 모름
-            localStorage.setItem(todoItem, JSON.stringify(obj));//API 1가지
-            this.todoItems.push(obj);
-    },
-    removeOneItem(todoItem, index) {
-      localStorage.removeItem(todoItem.item);
-      this.todoItems.splice(index,1); 
-    },
-    toggleOneItem(todoItem, index){
-        //todoItem.completed = !todoItem.completed;
-        this.todoItems[index].completed = !this.todoItems[index].completed;
-
-        localStorage.removeItem(todoItem.item);
-        localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-    },
-    clearAllItems() {
-      localStorage.clear();
-      this.todoItems = [];
-    }
-  },
-  created(){ //ready 같은의미
-    if (localStorage.length > 0){
-      for(let i =0; i < localStorage.length; i ++){
-        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-          console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-        //console.log(localStorage.key(i));
-      }
-    }
-  },
-  components:{
+ components:{
     //컴포넌트 태그명 : 컴포넌트내용
     TodoHeader,
     TodoInput,
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -28,7 +28,9 @@ export default {
         addTodo() {
             if(this.newTodoItem !== ''){
               // this.$emit('이벤트 이름',인자1, 인자2,...);
-            this.$emit('addTodoItem', this.newTodoItem);
+            //this.$emit('addTodoItem', this.newTodoItem);
+            //const text = this.newTodoItem.trim();
+            this.$store.commit('addOneItem', this.newTodoItem);
             this.clearInput(); 
             } else {
               this.showModal = !this.showModal;
